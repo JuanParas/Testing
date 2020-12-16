775e39bafeb338be4b147f4e6a6bf6c266f82f4a
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2020-12-16 17:05:06 -0500

    triangles OP

---
+++ tortuga.py
@@ -0,0 +1,24 @@
+import turtle
+
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+t.speed(10)
+for x in range(36):
+    t.pencolor(colors[x%6])
+    t.width(2)
+    t.forward(x)
+    t.left(120)
+t.left(120)
+for xx in range(5):
+    for x in range(36):
+        t.pencolor(colors[5-(x%6)])
+        t.width(2)
+        t.forward(35-x)
+        t.right(120)
+    t.left(60)
+    for x in range(36):
+        t.pencolor(colors[x%6])
+        t.width(2)
+        t.forward(x)
+        t.left(120)
