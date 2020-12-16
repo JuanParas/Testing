34103345b9e2acb3f1b926fabeb92274f1e31236
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2020-12-16 17:06:37 -0500

    commented loops

--- tortuga.py
+++ tortuga.py
@@ -4,12 +4,16 @@ colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
 t = turtle.Pen()
 turtle.bgcolor('black')
 t.speed(10)
+
+#Crea el primer triángulo
 for x in range(36):
     t.pencolor(colors[x%6])
     t.width(2)
     t.forward(x)
     t.left(120)
 t.left(120)
+
+#Loop para crear 5 triángulos más
 for xx in range(5):
     for x in range(36):
         t.pencolor(colors[5-(x%6)])
